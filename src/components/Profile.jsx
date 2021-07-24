b9688d2e1c6c362f01d6492c958eb559c727579c
tkachiko <tkachikos@gmail.com>
2021-07-24 13:12:28 +0300

    12. add Profile component

---
+++ src/components/Profile.jsx
@@ -0,0 +1,21 @@
+const Profile = () => {
+  return (
+    <main className='app-content'>
+      <div>
+        <img className='app-cover' src='https://vistapointe.net/images/reindeer-9.jpg' alt='cover-image' />
+      </div>
+      <div>avatar + description</div>
+      <div>
+        my posts
+        <div>new post</div>
+      </div>
+      <div>
+        posts
+        <div>post 1</div>
+        <div>post 2</div>
+      </div>
+    </main>
+  );
+};
+
+export default Profile;
