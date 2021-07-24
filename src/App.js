3cf9e7c78270d81f9ee4b07fcc5a33a53bdbf445
tkachiko <tkachikos@gmail.com>
2021-07-24 13:13:16 +0300

    12. import components

--- src/App.js
+++ src/App.js
@@ -1,56 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Profile from './components/Profile';
 
 function App() {
   return (
     <div className='app-wrapper'>
-      <header className='app-header'>
-        <img src={logo} className='app-logo' alt='logo' />
-      </header>
-      <nav className='app-nav'>
-        <ul className='list'>
-          <li className='list-item'>
-            <a className='link' href=''>
-              Profile
-            </a>
-          </li>
-          <li className='list-item'>
-            <a className='link' href=''>
-              Messages
-            </a>
-          </li>
-          <li className='list-item'>
-            <a className='link' href=''>
-              News
-            </a>
-          </li>
-          <li className='list-item'>
-            <a className='link' href=''>
-              Music
-            </a>
-          </li>
-          <li className='list-item'>
-            <a className='link' href=''>
-              Settings
-            </a>
-          </li>
-        </ul>
-      </nav>
-      <main className='app-content'>
-        <div>
-          <img className='app-cover' src='https://vistapointe.net/images/reindeer-9.jpg' alt='cover-image' />
-        </div>
-        <div>avatar + description</div>
-        <div>
-          my posts
-          <div>new post</div>
-        </div>
-        <div>
-          posts
-          <div>post 1</div>
-          <div>post 2</div>
-        </div>
-      </main>
+      <Header />
+      <Navbar />
+      <Profile />
     </div>
   );
 }
