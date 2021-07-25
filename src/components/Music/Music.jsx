770b32aa5cc337a708c36b814b0c53e33698c58c
tkachiko <tkachikos@gmail.com>
2021-07-25 14:17:05 +0300

    19. add News, Music, Settings components

---
+++ src/components/Music/Music.jsx
@@ -0,0 +1,7 @@
+import styles from './Music.module.css';
+
+const Music = () => {
+  return <div>Music</div>;
+};
+
+export default Music;
---
+++ src/components/News/News.jsx
@@ -0,0 +1,11 @@
+import styles from './News.module.css';
+
+const News = () => {
+  return (
+    <div>
+      News
+    </div>
+  );
+};
+
+export default News;
