1e1f83503265787197a4f4ddd265d62345e50fc6
tkachiko <tkachikos@gmail.com>
2021-07-25 19:41:52 +0300

    21. add basic layout for Dialogs page

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,7 +3,20 @@ import styles from './Dialogs.module.css';
 const Dialogs = props => {
   return (
     <div>
-      <div>Dialogs</div>
+      <div className={styles.dialogs}>
+        <div className={styles.dialogsItems}>
+          <div className={`${styles.dialog} ${styles.active}`}>Emmett</div>
+          <div className={styles.dialog}>Marty</div>
+          <div className={styles.dialog}>Lorraine</div>
+          <div className={styles.dialog}>George</div>
+          <div className={styles.dialog}>Jennifer</div>
+          <div className={styles.dialog}>Einstein</div>
+        </div>
+        <div className={styles.messages}>
+          <div className={styles.message}>Great Scott!</div>
+          <div className={styles.message}>Nobody calls me chicken!</div>
+        </div>
+      </div>
     </div>
   );
 };
