8d80a4e2f46ed5dcb32a472a3a1dd73ea52ab8cd
tkachiko <tkachikos@gmail.com>
2021-07-25 20:57:46 +0300

    24. UI-BLL, refactor Dialogs component

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,20 +3,34 @@ import styles from './Dialogs.module.css';
 import Message from './Message/Message';
 
 const Dialogs = props => {
+  let dialogsData = [
+    { id: 1, name: 'Emmett' },
+    { id: 2, name: 'Marty' },
+    { id: 3, name: 'Lorraine' },
+    { id: 4, name: 'George' },
+    { id: 5, name: 'Jennifer' },
+    { id: 6, name: 'Einstein' },
+  ];
+
+  let messagesData = [
+    { id: 1, message: 'Great Scott!' },
+    { id: 2, message: 'Nobody calls me chicken!' },
+  ];
+
   return (
     <div>
       <div className={styles.dialogs}>
         <div className={styles.dialogsItems}>
-          <DialogItem name='Emmett' id='1' />
-          <DialogItem name='Marty' id='2' />
-          <DialogItem name='Lorraine' id='3' />
-          <DialogItem name='George' id='4' />
-          <DialogItem name='Jennifer' id='5' />
-          <DialogItem name='Einstein' id='6' />
+          <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
+          <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />
+          <DialogItem name={dialogsData[2].name} id={dialogsData[2].id} />
+          <DialogItem name={dialogsData[3].name} id={dialogsData[3].id} />
+          <DialogItem name={dialogsData[4].name} id={dialogsData[4].id} />
+          <DialogItem name={dialogsData[5].name} id={dialogsData[5].id} />
         </div>
         <div className={styles.messages}>
-          <Message message='Great Scott!' />
-          <Message message='Nobody calls me chicken!' />
+          <Message message={messagesData[0].message} id={messagesData[0].id} />
+          <Message message={messagesData[1].message} id={messagesData[1].id} />
         </div>
       </div>
     </div>
