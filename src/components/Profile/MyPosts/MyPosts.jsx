1663476af88912a76d455a4ba5c98a94f5ab25fb
tkachiko <tkachikos@gmail.com>
2021-07-25 21:04:47 +0300

    24. UI-BLL, refactor MyPosts component

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,6 +2,11 @@ import styles from './MyPosts.module.css';
 import Post from './Post/Post';
 
 const MyPosts = () => {
+  const postsData = [
+    { id: 1, message: 'Hi how are you?', likesCount: 7 },
+    { id: 2, message: `It's my first post!`, likesCount: 15 },
+  ];
+
   return (
     <div className={styles.posts}>
       my posts
@@ -12,8 +17,8 @@ const MyPosts = () => {
         </div>
       </div>
       <div className={styles.item}>
-        <Post message="Hi how are you?" likesCount="7" />
-        <Post message="It's my first post!" likesCount="15" />
+        <Post message={postsData[0].message} likesCount={postsData[0].likesCount} id={postsData[0].id} />
+        <Post message={postsData[1].message} likesCount={postsData[1].likesCount} id={postsData[1].id} />
       </div>
     </div>
   );
