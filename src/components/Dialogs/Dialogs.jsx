bbaa1fc39e512b40bc51dce2fa99cb5567fc9f5c
tkachiko <tkachikos@gmail.com>
2021-07-25 23:11:46 +0300

    25. refactor Dialogs component using map method

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,7 +3,7 @@ import styles from './Dialogs.module.css';
 import Message from './Message/Message';
 
 const Dialogs = props => {
-  let dialogsData = [
+  const dialogs = [
     { id: 1, name: 'Emmett' },
     { id: 2, name: 'Marty' },
     { id: 3, name: 'Lorraine' },
@@ -12,26 +12,20 @@ const Dialogs = props => {
     { id: 6, name: 'Einstein' },
   ];
 
-  let messagesData = [
+  const messages = [
     { id: 1, message: 'Great Scott!' },
     { id: 2, message: 'Nobody calls me chicken!' },
   ];
 
+  const dialogsElements = dialogs.map(dialog => <DialogItem name={dialog.name} id={dialog.id} />);
+
+  const messagesElemnts = messages.map(message => <Message message={message.message} id={message.id} />);
+
   return (
     <div>
       <div className={styles.dialogs}>
-        <div className={styles.dialogsItems}>
-          <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
-          <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />
-          <DialogItem name={dialogsData[2].name} id={dialogsData[2].id} />
-          <DialogItem name={dialogsData[3].name} id={dialogsData[3].id} />
-          <DialogItem name={dialogsData[4].name} id={dialogsData[4].id} />
-          <DialogItem name={dialogsData[5].name} id={dialogsData[5].id} />
-        </div>
-        <div className={styles.messages}>
-          <Message message={messagesData[0].message} id={messagesData[0].id} />
-          <Message message={messagesData[1].message} id={messagesData[1].id} />
-        </div>
+        <div className={styles.dialogsItems}>{dialogsElements}</div>
+        <div className={styles.messages}>{messagesElemnts}</div>
       </div>
     </div>
   );
