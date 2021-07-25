bbd0e1b399747ca9957bac38a352dec4f078510d
tkachiko <tkachikos@gmail.com>
2021-07-25 20:02:28 +0300

    22. add NavLink in Dialogs page

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from 'react-router-dom';
 import styles from './Dialogs.module.css';
 
 const Dialogs = props => {
@@ -5,12 +6,36 @@ const Dialogs = props => {
     <div>
       <div className={styles.dialogs}>
         <div className={styles.dialogsItems}>
-          <div className={`${styles.dialog} ${styles.active}`}>Emmett</div>
-          <div className={styles.dialog}>Marty</div>
-          <div className={styles.dialog}>Lorraine</div>
-          <div className={styles.dialog}>George</div>
-          <div className={styles.dialog}>Jennifer</div>
-          <div className={styles.dialog}>Einstein</div>
+          <div className={styles.dialog}>
+            <NavLink to='/dialogs/1' className={styles.dialogLink} activeClassName={styles.active}>
+              Emmett
+            </NavLink>
+          </div>
+          <div className={styles.dialog}>
+            <NavLink to='/dialogs/2' className={styles.dialogLink} activeClassName={styles.active}>
+              Marty
+            </NavLink>
+          </div>
+          <div className={styles.dialog}>
+            <NavLink to='/dialogs/3' className={styles.dialogLink} activeClassName={styles.active}>
+              Lorraine
+            </NavLink>
+          </div>
+          <div className={styles.dialog}>
+            <NavLink to='/dialogs/4' className={styles.dialogLink} activeClassName={styles.active}>
+              George
+            </NavLink>
+          </div>
+          <div className={styles.dialog}>
+            <NavLink to='/dialogs/5' className={styles.dialogLink} activeClassName={styles.active}>
+              Jennifer
+            </NavLink>
+          </div>
+          <div className={styles.dialog}>
+            <NavLink to='/dialogs/6' className={styles.dialogLink} activeClassName={styles.active}>
+              Einstein
+            </NavLink>
+          </div>
         </div>
         <div className={styles.messages}>
           <div className={styles.message}>Great Scott!</div>
