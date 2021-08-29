cbce2b7456d7e098585f3185c64dd361cfdd7032
tkachiko <tkachikos@gmail.com>
2021-08-29 20:40:39 +0300

    98. captcha for Login page

--- src/components/Login/LoginPage.jsx
+++ src/components/Login/LoginPage.jsx
@@ -1,23 +1,60 @@
+import { Formik } from 'formik';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
-import { login } from '../../redux/auth-reducer';
+import * as Yup from 'yup';
+import { login } from './../../redux/auth-reducer';
 import LoginForm from './LoginForm';
 import styles from './LoginPage.module.css';
 
-const LoginPage = ({isAuth}) => {
+const LoginPage = ({ isAuth, captchaUrl, error, login }) => {
   if (isAuth) {
     return <Redirect to='/profile' />;
   }
   return (
     <div className={styles.loginForm}>
       <h1>Sign in</h1>
-      <LoginForm login={login} />
+      <Formik
+        initialValues={{
+          email: '',
+          password: '',
+          rememberMe: false,
+          captcha: '',
+        }}
+        validationSchema={Yup.object({
+          email: Yup.string().required('Required'),
+          password: Yup.string().required('Required'),
+        })}
+        onSubmit={(values, { setSubmitting, setFieldError, setStatus }) => {
+          login(
+            values.email,
+            values.password,
+            values.rememberMe,
+            values.captcha,
+            setSubmitting,
+            setFieldError,
+            setStatus,
+          );
+          console.log('Logging in as', values.email);
+          setSubmitting(false);
+        }}
+      >
+        {({ isSubmitting, status }) => (
+          <LoginForm
+            captchaUrl={captchaUrl}
+            error={error}
+            isSubmitting={isSubmitting}
+            status={status}
+          />
+        )}
+      </Formik>
     </div>
   );
 };
 
 const mapStateToProps = state => ({
+  captchaUrl: state.auth.captchaUrl,
+  error: state.auth.error,
   isAuth: state.auth.isAuth,
 });
 
-export default connect(mapStateToProps, null)(LoginPage);
+export default connect(mapStateToProps, { login })(LoginPage);
--- src/redux/auth-reducer.js
+++ src/redux/auth-reducer.js
@@ -1,7 +1,8 @@
-import { authAPI } from '../API/API';
+import { authAPI, securityAPI } from '../API/API';
 
 const SET_USER_DATA = 'auth/SET_USER_DATA';
-const STOP_SUBMIT = 'STOP_SUBMIT';
+const STOP_SUBMIT = 'auth/STOP_SUBMIT';
+const GET_CAPTCHA_URL_SUCCESS = 'auth/GET_CAPTCHA_URL_SUCCESS';
 
 const initialState = {
   userId: null,
@@ -9,17 +10,19 @@ const initialState = {
   login: null,
   isAuth: false,
   error: null,
+  captchaUrl: null, // if null, then captcha is not required
 };
 
 const authReducer = (state = initialState, action) => {
   switch (action.type) {
     case SET_USER_DATA:
+    case GET_CAPTCHA_URL_SUCCESS:
       return {
         ...state,
         ...action.payload,
       };
-      case STOP_SUBMIT:
-        return {...state, error: action.error };
+    case STOP_SUBMIT:
+      return { ...state, error: action.error };
     default:
       return state;
   }
@@ -30,8 +33,11 @@ export const setAuthUserData = (userId, login, email, isAuth) => ({
   type: SET_USER_DATA,
   payload: { userId, login, email, isAuth },
 });
-
-export const stopSubmit = (error) => ({ type: STOP_SUBMIT, error });
+export const stopSubmit = error => ({ type: STOP_SUBMIT, error });
+export const getCaptchaUrlSucces = captchaUrl => ({
+  type: GET_CAPTCHA_URL_SUCCESS,
+  payload: { captchaUrl },
+});
 
 // thunk creators
 export const getAuthUserData = () => async dispatch => {
@@ -43,14 +49,31 @@ export const getAuthUserData = () => async dispatch => {
 };
 
 export const login =
-  (email, password, rememberMe, setStatus) => async dispatch => {
-    const response = await authAPI.login(email, password, rememberMe);
+  (email, password, rememberMe, captcha, setStatus) => async dispatch => {
+    const response = await authAPI.login(email, password, rememberMe, captcha);
     if (response.data.resultCode === 0) {
+      // success, get auth data
       dispatch(getAuthUserData());
+      console.log('Logged in successfully!');
     } else {
+      if (response.data.resultCode === 10) {
+        dispatch(getCaptchaUrl());
+      }
+      let error =
+        response.data.messages.length > 0
+          ? response.data.messages[0]
+          : 'Some error';
+      dispatch(stopSubmit(error));
       setStatus(response.data.messages);
+      console.log('An error has occured');
     }
   };
+
+export const getCaptchaUrl = () => async dispatch => {
+  const response = await securityAPI.getCaptchaUrl();
+  const captchaUrl = response.data.url;
+  dispatch(getCaptchaUrlSucces(captchaUrl));
+};
 
 export const logout = () => async dispatch => {
   const response = await authAPI.logout();
